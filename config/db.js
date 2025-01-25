67bb2fb2fb6c4c9eb7cdd6e0b593bc2d5f019d90
Palkesh Soni <sonipalkesh43@gmail.com>
2025-01-25 11:36:46 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -4,11 +4,11 @@ const colors = require('colors')
 const connectDB = async()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL);
-        console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white)
+        // console.log(`Mongodb connected ${mongoose.connection.host}`.bgGreen.white)
     }
     catch (error)
     {
-        console.log(`MongoDB server issue ${error}`.bgRed.white)
+        // console.log(`MongoDB server issue ${error}`.bgRed.white)
     }
 }
 
